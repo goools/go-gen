810941d6f59a7ba69667d00cefd6dbfc00036aa3
siskinc <susecjh@gmail.com>
2020-12-14 19:39:08 +0800

    Feature: [syncpool] complete code

--- generate/syncpool_generator/generator.go
+++ generate/syncpool_generator/generator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/dave/jennifer/jen"
 	"github.com/goools/go-gen/generate"
 	"github.com/sirupsen/logrus"
 )
@@ -51,5 +52,74 @@ func (syncPool *SyncPool) WriteToFile() {
 	logrus.Infof("begin generate sync pool: %s", syncPool.Name)
 	syncPoolSnackName := generate.ToSnakeCase(syncPool.Name)
 	syncPoolFileName := fmt.Sprintf("%s_syncpool_generate.go", syncPoolSnackName)
-	logrus.Infof("file: %s", syncPoolFileName)
+
+	generateFile := jen.NewFilePath(syncPool.PkgPath)
+	generateFile.HeaderComment(generate.WriteDoNotEdit())
+
+	generateFile.Add(syncPool.writeTypeDef())
+	generateFile.Line()
+	generateFile.Add(syncPool.writeFuncPut())
+	generateFile.Line()
+	generateFile.Add(syncPool.writeFuncGet())
+
+	err := generateFile.Save(syncPoolFileName)
+	if err != nil {
+		logrus.Fatalf("save syncpool code to file have an err: %v, syncpool: %s, file: %s", err, syncPool.Name, syncPool)
+	}
+	logrus.Infof("complete generate syncpool: %s", syncPool.Name)
+}
+
+func (syncPool *SyncPool) writeTypeDef() jen.Code {
+	res := jen.Type()
+	res.Id(syncPool.Name).Qual("sync", "Pool")
+	return res
+}
+
+func (syncPool *SyncPool) ValueType() jen.Code {
+	return syncPool.Value.Code()
+}
+
+func (syncPool *SyncPool) PreFuncCommon(funcName string) *jen.Statement {
+	return jen.Func().
+		Params(jen.Id(syncPool.funcThisName).Add(jen.Id(fmt.Sprintf("*%s", syncPool.Name)))).Id(funcName)
+}
+
+func (syncPool *SyncPool) ObjCode() *jen.Statement {
+	ptrOp := jen.Op("*")
+	objCode := jen.Params(ptrOp.Add(jen.Qual("sync", "Pool"))).Params(jen.Id(syncPool.funcThisName))
+	return objCode
+}
+
+func (syncPool *SyncPool) writeFuncPut() jen.Code {
+	res := syncPool.PreFuncCommon("Put")
+
+	// func params
+	res = res.Params(jen.Id("value").Add(syncPool.ValueType()))
+
+	// func body
+	objCode := syncPool.ObjCode()
+	res = res.Block(
+		objCode.Dot("Put").Call(jen.Id("value")),
+	)
+
+	return res
+}
+
+func (syncPool *SyncPool) writeFuncGet() jen.Code {
+	res := syncPool.PreFuncCommon("Get")
+
+	// func params
+	res = res.Params()
+
+	// func resp
+	res = res.Params(syncPool.ValueType())
+
+	// func body
+	objCode := syncPool.ObjCode()
+	res = res.Block(
+		jen.Id("value").Op(":=").Add(objCode.Dot("Get").Call()),
+		jen.Return(jen.Id("value").Assert(syncPool.ValueType())),
+	)
+
+	return res
 }
--- test/syncpool/gen.go
+++ test/syncpool/gen.go
@@ -1,3 +1,3 @@
 package syncpool
 
-//go:generate go run ../../main.go -v gen syncpool Pill3<encoding/json.Number>
+//go:generate go run ../../main.go -v gen syncpool Pill<int> Pill2<string> Pill3<encoding/json.Number> Pill4<Pill>
